32afebf1173f3edc5e4b54f96f95be858f4e20a0
tonuidavies <tonuidavies@gmail.com>
2021-02-14 01:04:24 +0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+const App = ()=> {
+  return (
+   <>
+   <h1> Welcome to proshop</h1>
+   </>
+  );
+}
+
+export default App;
