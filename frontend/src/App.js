e9652584632d5779fe476ae7cd63bde7d43e7578
tonuidavies <tonuidavies@gmail.com>
2021-03-04 22:44:33 +0300

    Users list dashboard frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
 
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
      <Route  path='/profile' component={ProfileScreen} />
      <Route  path='/product/:id' component={ProductScreen} />
      <Route  path='/cart/:id' component={CartScreen} />
+     <Route  path='/userlist' component={UserListScreen} />
      <Route  path='/' component={HomeScreen} exact />
 
 
